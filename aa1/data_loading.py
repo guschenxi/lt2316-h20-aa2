f9ed431adeae47cc458663e4430b1227846e4a1e
guschenxi <chenxijack@gmail.com>
2020-10-13 14:46:25 +0200

    replace aa1

--- aa1/data_loading.py
+++ aa1/data_loading.py
@@ -3,7 +3,14 @@
 import random
 import pandas as pd
 import torch
+import glob
+import xml.etree.ElementTree as et
+import numpy as np
+import matplotlib.pyplot as plt
+import string
 
+
+import os
 
 class DataLoaderBase:
 
@@ -14,16 +21,16 @@ class DataLoaderBase:
         assert list(self.data_df.columns) == [
                                                 "sentence_id",
                                                 "token_id",
-                                                "char_start_idx",
-                                                "char_end_idx",
-                                                "split" # e.g. TEST, TRAIN, VAL
+                                                "char_start_id",
+                                                "char_end_id",
+                                                "split"
                                                 ]
 
         assert list(self.ner_df.columns) == [
                                                 "sentence_id",
                                                 "ner_id",
-                                                "char_start_idx",
-                                                "char_end_idx",
+                                                "char_start_id",
+                                                "char_end_id",
                                                 ]
         self.device = device
         
@@ -69,6 +76,117 @@ class DataLoader(DataLoaderBase):
         # NOTE! I strongly suggest that you create multiple functions for taking care
         # of the parsing needed here. Avoid create a huge block of code here and try instead to 
         # identify the seperate functions needed.
+        data_df_cols = ["sentence_id", "token_id", "char_start_id", "char_end_id", "split"]
+        data_df_rows = []
+
+        ner_df_cols = ["sentence_id", "ner_id", "char_start_id", "char_end_id"]
+        ner_df_rows = []
+
+        vocab={}
+        ner_dict={'O': 0, 'drug': 1, 'group': 2, 'brand': 3, 'drug_n': 4}
+
+        #parse xml
+        path=[['TRAIN','*/*/*.xml'],['TEST','*/Test for DrugNER task/*/*.xml']]
+        for path in path:
+            for file in glob.iglob(os.path.join(data_dir, path[1])):
+                with open(file) as f:
+                    xtree = et.parse(f)
+                    xroot = xtree.getroot()
+
+                for senten in xroot:
+                    sentence_id = senten.attrib.get("id")
+                    sentence = senten.attrib.get("text")
+                    for token in sentence.lower().strip().split():
+                        token=''.join((char for char in token if char not in string.punctuation)).strip()
+                        if token.strip() == "":
+                            continue
+                        if token not in vocab:
+                            vocab[token]=len(vocab)
+                        token_id = vocab[token]
+                        char_start_id = sentence.lower().find(token)
+                        data_df_rows.append({"sentence_id": sentence_id, "token_id": token_id, 
+                                    "char_start_id": char_start_id, "char_end_id": char_start_id+len(token)-1,
+                                    "split": path[0]})
+
+                    for node in senten:
+                        if node.tag == 'entity':
+                            type_name = node.attrib.get("type")
+                            if type_name in ner_dict: ner_id = ner_dict[type_name]
+                            else: ner_id = 0
+                            entity_name = node.attrib.get("text")
+                            if len(entity_name.split(" ")) == 1: #entity name consists of only one word
+                                ner_id = ner_dict[node.attrib.get("type")]
+                                if ';' in node.attrib.get("charOffset"): # Deal with special format in Train/DrugBank/Eszopiclone_ddi.xml
+                                    charOffset = [entity_name.find(token), entity_name.find(token) + len(token)-1]
+                                else:
+                                    charOffset = node.attrib.get("charOffset").split("-")
+                                ner_df_rows.append({"sentence_id": sentence_id, "ner_id": ner_id, 
+                                        "char_start_id": int(charOffset[0]), "char_end_id": int(charOffset[1]),
+                                        })
+                            else: #entity name consists of more than one word
+                                for token in entity_name.split(" "):
+                                    ner_id = ner_dict[node.attrib.get("type")]
+                                    char_start_id = entity_name.find(token)
+                                    ner_df_rows.append({"sentence_id": sentence_id, "ner_id": ner_id, 
+                                        "char_start_id": char_start_id, "char_end_id": char_start_id + len(token),
+                                        })
+
+        #create dataframe
+        self.data_df = pd.DataFrame(data_df_rows, columns = data_df_cols)
+        self.ner_df = pd.DataFrame(ner_df_rows, columns = ner_df_cols)
+        
+        
+            
+        #divide VAL NEW
+        train_set=self.data_df[self.data_df["split"]=="TRAIN"]
+        train_ids=train_set["sentence_id"].unique()
+        val_ids = np.random.choice(train_ids, size = int(len(train_ids) * 0.3))
+        for ids in val_ids:
+            self.data_df.loc[(self.data_df['sentence_id'] == ids),'split'] = 'VAL'
+        
+        self.id2word={value : key for (key, value) in vocab.items()}
+        self.id2ner={value : key for (key, value) in ner_dict.items()}
+        self.vocab=[[key,value] for key, value in vocab.items()]
+        
+        
+        #create X and Y data
+        self.train_sentences= []
+        self.train_labels= []
+        self.val_sentences= []
+        self.val_labels= []
+        self.test_sentences= []
+        self.test_labels= []
+        for sen_id in list(self.data_df["sentence_id"].unique()):
+            s_tokens = self.data_df[self.data_df["sentence_id"]==sen_id]
+            s_ners = self.ner_df[self.ner_df["sentence_id"]==sen_id]
+            sentence=[]
+            label = []
+            for i,t_row in s_tokens.iterrows():
+                sentence.append(t_row["token_id"])
+                is_ner = False
+                for i, l_row in s_ners.iterrows():
+                    if t_row["char_start_id"] >= l_row["char_start_id"] and t_row["char_start_id"] <= l_row["char_end_id"]:
+                        label.append(l_row["ner_id"])
+                        is_ner = True
+                if not is_ner:
+                    label.append(0)
+
+              #Split
+            split=s_tokens['split'].unique().tolist()[0]
+            if split == "TRAIN":
+                self.train_labels.append(label)
+                self.train_sentences.append(sentence)
+            elif split == "VAL":
+                self.val_labels.append(label)
+                self.val_sentences.append(sentence)
+            elif split == "TEST":
+                self.test_labels.append(label)
+                self.test_sentences.append(sentence)
+        a=max([len(i) for i in self.train_sentences])
+        b=max([len(i) for i in self.val_sentences])
+        c=max([len(i) for i in self.test_sentences])
+        self.max_sample_length = max([a,b,c])
+        #print ("self.max_sample_length",a,b,c,self.max_sample_length)
         pass
 
 
@@ -77,16 +195,52 @@ class DataLoader(DataLoaderBase):
         # the tensors should have the following following dimensions:
         # (NUMBER_SAMPLES, MAX_SAMPLE_LENGTH)
         # NOTE! the labels for each split should be on the GPU
-        pass
+        
+        
+        #OLD why not working?
+        #[x.extend((self.max_sample_length-len(x))*[-1]) for x in self.train_labels]
+        #[x.extend((self.max_sample_length-len(x))*[-1]) for x in self.val_labels]
+        #[x.extend((self.max_sample_length-len(x))*[-1]) for x in self.test_labels]
+        x=[(i + [-1] * self.max_sample_length)[:self.max_sample_length] for i in self.train_labels]
+        y=[(i + [-1] * self.max_sample_length)[:self.max_sample_length] for i in self.val_labels]
+        z=[(i + [-1] * self.max_sample_length)[:self.max_sample_length] for i in self.test_labels]
+        train_labels_tensor=torch.Tensor(x).to(device=self.device)
+        val_labels_tensor=torch.Tensor(y).to(device=self.device)
+        test_labels_tensor=torch.Tensor(z).to(device=self.device)
+        output_data=[train_labels_tensor, val_labels_tensor, test_labels_tensor]
+        return output_data
+
 
     def plot_split_ner_distribution(self):
         # should plot a histogram displaying ner label counts for each split
+        ner_counts_train = sum([sum(y>0 for y in x) for x in zip(*self.train_labels)])
+        ner_counts_val = sum([sum(y>0 for y in x) for x in zip(*self.val_labels)])
+        ner_counts_test = sum([sum(y>0 for y in x) for x in zip(*self.test_labels)])
+
+        x=[ner_counts_train, ner_counts_val, ner_counts_test]
+        y=["Train","Validation","Test"]
+        plt.title("NER label counts for each split")
+        plt.xlabel('Splits')
+        plt.ylabel('Counts')
+        plt.bar(y,x,color='rgb',tick_label=y)
+        plt.show()
         pass
 
 
     def plot_sample_length_distribution(self):
         # FOR BONUS PART!!
         # Should plot a histogram displaying the distribution of sample lengths in number tokens
+        length_train = [len(x) for x in self.train_labels]
+        length_val = [len(x) for x in self.val_labels]
+        length_test = [len(x) for x in self.test_labels]
+        
+        x=length_train + length_val + length_test
+        fig,ax = plt.subplots(1,1)
+        ax.set_title("Distribution of sample lengths")
+        ax.set_xlabel('Sample Lengths')
+        ax.set_ylabel('Counts')
+        plt.hist(x)
+        plt.show()
         pass
 
 
@@ -94,6 +248,18 @@ class DataLoader(DataLoaderBase):
         # FOR BONUS PART!!
         # Should plot a histogram displaying the distribution of number of NERs in sentences
         # e.g. how many sentences has 1 ner, 2 ner and so on
+        Ner_count_train = [sum(y>0 for y in x) for x in self.train_labels]
+        Ner_count_val = [sum(y>0 for y in x) for x in self.val_labels]
+        Ner_count_test = [sum(y>0 for y in x) for x in self.test_labels]
+        
+        x = Ner_count_train + Ner_count_val + Ner_count_test
+        counts,values = pd.Series(x).value_counts().values, pd.Series(x).value_counts().index
+        plt.figure(figsize=(15,5))
+        plt.title("Distribution of number of NERs in sentences")
+        plt.xlabel('Number of NERs in sentences')
+        plt.ylabel('Counts')
+        plt.bar(values,counts,tick_label=values)
+        plt.show()
         pass
 
 
--- aa1/feature_extraction.py
+++ aa1/feature_extraction.py
@@ -1,11 +1,14 @@
 
 #basics
 import pandas as pd
+import torch
+import string
+alphabet=dict(zip(string.ascii_lowercase, range(1, 27)))
 
 # Feel free to add any new code to this script
 
 
-def extract_features(data:pd.DataFrame, max_sample_length:int):
+def extract_features(data:pd.DataFrame, max_sample_length:int, device, id2word):
     # this function should extract features for all samples and 
     # return a features for each split. The dimensions for each split
     # should be (NUMBER_SAMPLES, MAX_SAMPLE_LENGTH, FEATURE_DIM)
@@ -13,4 +16,77 @@ def extract_features(data:pd.DataFrame, max_sample_length:int):
     #
     # NOTE! Feel free to add any additional arguments to this function. If so
     # document these well and make sure you dont forget to add them in run.ipynb
-    pass
+    train_sentences= []
+    val_sentences= []
+    test_sentences= []
+
+    for sen_id in list(data["sentence_id"].unique()):
+        sentence=[]
+        s_tokens = data[data["sentence_id"]==sen_id]
+        
+        seq=list(set(s_tokens['token_id']))
+                 
+        for item in seq:
+            #Get token_id
+            token_id=item
+            word=id2word[item]
+            #print(word, len(word))
+                 
+            #Get word length
+            word_len=len(word)
+            if word_len==0:
+                continue
+            
+            #Get last character
+            if word[-1] in string.ascii_lowercase:
+                last_char=alphabet[word[-1]]
+            else:
+                last_char=0
+            
+            #Get previous token_id and next token_id
+            if seq.index(token_id) > 0:
+                 pre_token_id = seq[seq.index(token_id)-1]
+            else:
+                 pre_token_id = 0
+            if seq.index(token_id) < len(seq)-1:
+                 next_token_id = seq[seq.index(token_id)+1]
+            else:
+                 next_token_id = 0
+                 
+            sentence.append([token_id, 
+                             pre_token_id, 
+                             next_token_id, 
+                             word_len, 
+                             last_char
+                            ])
+
+           
+        split=s_tokens['split'].unique().tolist()[0]
+        if split == "TRAIN":
+            train_sentences.append(sentence)
+        elif split == "VAL":
+            val_sentences.append(sentence)
+        elif split == "TEST":
+            test_sentences.append(sentence)
+            
+        C=[-1,-1,-1,-1,-1]
+        for x in train_sentences:
+            for i in range(max_sample_length-len(x)):
+                x.append(C)
+        for y in val_sentences:
+            for i in range(max_sample_length-len(y)):
+                y.append(C)
+        for z in test_sentences:
+            for i in range(max_sample_length-len(z)):
+                z.append(C)
+    
+    train_sentences_tensor=torch.Tensor(train_sentences).to(device=device)
+    val_sentences_tensor=torch.Tensor(val_sentences).to(device=device)
+    test_sentences_tensor=torch.Tensor(test_sentences).to(device=device)
+
+    output_data=[train_sentences_tensor, val_sentences_tensor, test_sentences_tensor]
+    print("TRAIN Tensor Size:", train_sentences_tensor.size())
+    print("VAL Tensor Size:", val_sentences_tensor.size())
+    print("TEST Tensor Size:", test_sentences_tensor.size())
+    return output_data
+    
